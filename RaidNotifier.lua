36bae2579c40848a1c69f62c7d6c1f3af5957b13
= <memusek@gmail.com>
2017-07-15 15:52:40 +0200

    Hide ultimates window when there is no group

--- RaidNotifier.lua
+++ RaidNotifier.lua
@@ -270,7 +270,12 @@ do ----------------------
 		-- UPDATE: should no longer matter
 		local function OnGroupUpdate()
 			local newMembers = {}
-			for i=1, GetGroupSize() do
+			local groupSize = GetGroupSize()
+			if (groupSize == 0) then
+				UI.SetElementHidden("ultimate", "ulti_window", true)
+				return
+			end
+			for i=1, groupSize do
 				local userName = GetUnitDisplayName("group"..i)
 				if userName and userName ~= "" then 
 					newMembers[userName] = IsUnitOnline("group"..i)
