3ced31d1884e417ab582fa78eb9ae2683ea9bb1d
memus <memusek@gmail.com>
2017-09-13 07:47:42 +0000

    Added tick-tock terrorizer replacement for pana when he get his achiv

--- libs/LibCustomTitles/Titles/Memus.lua
+++ libs/LibCustomTitles/Titles/Memus.lua
@@ -11,6 +11,7 @@ if not MY_MODULE then return end
 MY_MODULE:RegisterTitle("@memus", nil, 92, {en = "Paid Healer"}, {color={"#22E500", "#BF0000"}})
 MY_MODULE:RegisterTitle("@memus", nil, 93, {en = "Vitality Bonus Increaser"}, {color={"#052FCC", "#12CCDE"}})
 MY_MODULE:RegisterTitle("@Panadulek", nil, 92, {en = "Pyrotechnic Dragon Lord"}, {color={"#F5C514", "#D20F08"}})
+MY_MODULE:RegisterTitle("@Panadulek", nil, 1838, {en = "Tick-Tock Terrorizer"}, {color={"#F5C514", "#D20F08"}})
 MY_MODULE:RegisterTitle("@Lancast'X", nil, 92, {en = "Toxic Player"}, {color="#00a9e7"})
 MY_MODULE:RegisterTitle("@Zathurin", nil, 702, {en = "Tempest Warlock"}, {color={"#009AE5", "#6C12CD"}})
 --MY_MODULE:RegisterTitle("@Zedh", nil, 92, {en = "Catfish Queen", de = "Kampfi's Liebling"}, {color={"#65F1C9","#D6C800"}})
