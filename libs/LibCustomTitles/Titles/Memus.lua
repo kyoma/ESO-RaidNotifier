5efcb07f54790c5c851cae5155cd914a3b5c8064
memus <memusek@gmail.com>
2018-10-27 20:40:29 +0200

    Changes in titles

--- libs/LibCustomTitles/Titles/Memus.lua
+++ libs/LibCustomTitles/Titles/Memus.lua
@@ -26,3 +26,4 @@ MY_MODULE:RegisterTitle("@kilnerdyne", nil, 92, {en = "Late Night Master"}, {col
 MY_MODULE:RegisterTitle("@LiofaTR", nil, 92, {en = "The Tonk"}, {color={"#F2726E", "#C40808"}})
 MY_MODULE:RegisterTitle("@XiaoWeiXiao", nil, 92, {en = "|cE70000To|r|cFF8C00ma|r|cFFEF00to|r|c00811F|sh|r|c0044FFar|r|c760089k|r"})
 MY_MODULE:RegisterTitle("@WHoitsma", nil, 92, {en = "|cE70000The|r |cFF8C00God|r |cFFEF00of|r |c00811F|Fem|r|c0044FFini|r|c760089sts|r"})
+MY_MODULE:RegisterTitle("@getemshauna", nil, 1838, {en = "Grand Master Impaler"}, {color="#00FF00"})
