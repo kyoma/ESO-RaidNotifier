aa018bd5fec3b7c6971e85535bae0f940b01e6b4
Arrvis the Lion <dracoignitus@gmail.com>
2021-03-24 20:31:12 +0300

    Version 2.11.11 by Kyoma

--- RaidNotifier.lua
+++ RaidNotifier.lua
@@ -5,7 +5,7 @@ local RaidNotifier = RaidNotifier
 
 RaidNotifier.Name           = "RaidNotifier"
 RaidNotifier.DisplayName    = "Raid Notifier"
-RaidNotifier.Version        = "2.11.10"
+RaidNotifier.Version        = "2.11.11"
 RaidNotifier.Author         = "|c009ad6Kyoma, Memus, Woeler, silentgecko|r"
 RaidNotifier.SV_Name        = "RNVars"
 RaidNotifier.SV_Version     = 4
