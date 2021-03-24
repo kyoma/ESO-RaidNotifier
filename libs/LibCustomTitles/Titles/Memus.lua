0efd82685bcb608fbb449224cd6c727eba33bea2
Arrvis the Lion <dracoignitus@gmail.com>
2021-03-24 20:26:22 +0300

    Version 2.11.10 by memus sync (esoui.com version applied)

--- libs/LibCustomTitles/Titles/Memus.lua
+++ libs/LibCustomTitles/Titles/Memus.lua
@@ -14,6 +14,7 @@ MY_MODULE:RegisterTitle("@Panadulek", nil, 92, {en = "Exploitdulek"}, {color={"#
 MY_MODULE:RegisterTitle("@Panadulek", nil, 2139, {en = "Gryphon Heart"}, {color={"#8305D6", "#CB0BA0"}})
 MY_MODULE:RegisterTitle("@Panadulek", nil, 93, {en = "Pyrotechnic Dragon Lord"}, {color={"#F5C514", "#D20F08"}})
 MY_MODULE:RegisterTitle("@iN_Lancast", nil, 92, {en = "|cE70000Un|r|cFF8C00de|r|cFFEF00r T|r|c00811Fhe|r |c0044FFMe|r|c760089sh|r"})
+MY_MODULE:RegisterTitle("@Carlos2300", nil, 92, {en = "TheOneAndOnlyRP"}, {color={"FF0090", "#FF0000"}})
 MY_MODULE:RegisterTitle("@Heelie", nil, 92, {en = "The Snake"}, {color="#00FF00"})
 MY_MODULE:RegisterTitle("@Heelie", nil, 1391, {en = "The Snake"}, {color="#FF0000"})
 MY_MODULE:RegisterTitle("@kilnerdyne", nil, 92, {en = "Late Night Master"}, {color={"#2277BB", "#005599"}})
@@ -24,5 +25,3 @@ MY_MODULE:RegisterTitle("@pyromaaniac", nil, 92, {en = "CEO of Jabbing"}, {color
 MY_MODULE:RegisterTitle("@Razall731", nil, 92, {en = "|cff0000K|r|cff2a00r|r|cff5500e|r|cff7f00m|r|cffbf00ó|r |cffff00w|r|caaff00k|r|c55ff00o|r|c00ff00w|r|c00ff80y|r|c00ffff|r |c00aaffB|r|c0055ffa|r|c0000ffr|r|c4600ffo|r|c8b00ffn|r"})
 MY_MODULE:RegisterTitle("@Pandross", nil, 92, {en = "Gryphon Fart"}, {color={"#33CCFF", "#66FF33"}})
 MY_MODULE:RegisterTitle("@qwertzuiop0815", nil, 92, {en = "savage"}, {color={"#791B49", "#355549"}})
-
-
