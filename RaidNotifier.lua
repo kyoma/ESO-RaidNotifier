2b0f5bbc27330fe856386720d99c415a9f0af3d9
Pavel Dovlatov <mysterydragon@yandex.com>
2021-03-25 21:58:04 +0300

    Marked version 2.16

--- RaidNotifier.lua
+++ RaidNotifier.lua
@@ -5,7 +5,7 @@ local RaidNotifier = RaidNotifier
 
 RaidNotifier.Name           = "RaidNotifier"
 RaidNotifier.DisplayName    = "Raid Notifier"
-RaidNotifier.Version        = "2.15"
+RaidNotifier.Version        = "2.16"
 RaidNotifier.Author         = "|c009ad6Kyoma, Memus, Woeler, silentgecko|r"
 RaidNotifier.SV_Name        = "RNVars"
 RaidNotifier.SV_Version     = 4
